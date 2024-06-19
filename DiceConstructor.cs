efecf0775f95957e4922971083729478b2f18e01
ostracized605 <kolco0202@gmail.com>
2024-06-19 21:05:02 +0700

    finished construcion of the object[] dice array

--- DiceConstructor.cs
+++ DiceConstructor.cs
@@ -6,35 +6,42 @@ namespace DiceThrowing{
         const int height=8;
         const int width=16;
         private delegate void GeneralDieConstructCondition(int i, int j,ref string[,] array,int height=8,int width=16);
+        private delegate void DieConstructCondition(int i, int j,int x, string[,] array,int height=8,int width=16);
         ///probably array[,]?
         public static void DieConstructor(){
             
             string[,] die=new string[height,width];    ///i = row, j = column
             object[] dice=new object[6];
-            ConstructDie(die);
+            
+            ConstructDie(die,dice);
         }
         
-        public static void ConstructDie(string[,] die,int height=8,int width=16){
-            DieConstructorConditions DCC=new DieConstructorConditions();
+        public static void ConstructDie(string[,] die,object[] dice,int height=8,int width=16){
             ConstructLoop(DieConstructorConditions.ConstructDie_GeneralConditions,die);
-            ConstructLoop_Numbers(DieConstructorConditions.ConstructDie_Conditions6,die);
+            
+            for (int i=0;i<6;i++){
+                    string[,] array=(string[,])die.Clone();
+                    dice[i]=ConstructLoop_Numbers(DieConstructorConditions.ConstructDie_Conditions,array,i+1);
+                }
+            PrintDice(dice);
+        }
 
-            PrintDie(die);
 
-            static void ConstructLoop(GeneralDieConstructCondition method,string[,] array,int height=8,int width=16){
-                for (int i=0;i<height;i++){
-                    for (int j=0;j<width;j++){
-                        method(i, j,ref array);
-                    }
+        static void ConstructLoop(GeneralDieConstructCondition method,string[,] array,int height=8,int width=16){
+            for (int i=0;i<height;i++){
+                for (int j=0;j<width;j++){
+                    method(i, j,ref array);
                 }
             }
-            static void ConstructLoop_Numbers(GeneralDieConstructCondition method,string[,] array,int height=8,int width=16){
-                for (int i=2;i<height-1;i++){
-                    for (int j=3;j<width-5;j++){
-                        method(i, j,ref array);
-                    }
+        }
+        static object ConstructLoop_Numbers(DieConstructCondition method,string[,] array,int x,int height=8,int width=16){
+            //string[,] array1=array;
+            for (int i=2;i<height-1;i++){
+                for (int j=3;j<width-5;j++){
+                    method(i, j,x, array);
                 }
             }
+            return array;
         }
         static void PrintDie(string[,] array,int height=8,int width=16){
             for (int i=0;i<height;i++){
@@ -44,6 +51,12 @@ namespace DiceThrowing{
                 System.Console.WriteLine();
             }
         }
-
+        static void PrintDice(object[] array,int height=8,int width=16){
+            for(int i=0;i<6;i++){
+                System.Console.WriteLine();
+                string[,] die=(string[,])array[i];
+                PrintDie(die);
+            } 
+        }
     }
 }
--- DiceConstructorConditions.cs
+++ DiceConstructorConditions.cs
@@ -22,126 +22,246 @@ namespace DiceThrowing{
             }
         }
 
-        internal static void ConstructDie_Conditions1(int i, int j,ref string[,] array,int height=8,int width=16){
-            if (i==2||i==4||i==5){
-                if(j>=7&&j<=8){
-                    array[i,j]="0";
-                }
-            }
-            if(i==3){
-                if(j>=5&&j<=8){
-                    array[i,j]="0";
-                }
-            }
-            if(i==6){
-                if(j>=5&&j<=10){
-                    array[i,j]="0";
-                }
-            }
-        }
-        
-        internal static void ConstructDie_Conditions2(int i, int j,ref string[,] array,int height=8,int width=16){
-
-            switch(i){
+        internal static void ConstructDie_Conditions(int i, int j,int x, string[,] array,int height=8,int width=16){
+            switch(x){
+                case 1:
+                    if (i==2||i==4||i==5){
+                        if(j>=7&&j<=8){
+                            array[i,j]="0";
+                        }
+                    }
+                    if(i==3){
+                        if(j>=5&&j<=8){
+                            array[i,j]="0";
+                        }
+                    }
+                    if(i==6){
+                        if(j>=5&&j<=10){
+                            array[i,j]="0";
+                        }
+                    }
+                break;
                 case 2:
-                    if(j>=6&&j<=9){
-                    array[i,j]="0";
-                }
+                    switch(i){
+                        case 2:
+                            if(j>=6&&j<=9){
+                            array[i,j]="0";
+                        }
+                        break;
+                        case 3:
+                            if (j==5||j==6||j==9||j==10){
+                            array[i,j]="0";
+                        }
+                        break;
+                        case 4:
+                            if(j==8||j==9){
+                            array[i,j]="0";
+                        }
+                        break;
+                        case 5:
+                            if(j==7||j==8){
+                            array[i,j]="0";
+                        }
+                        break;
+                        case 6:
+                            if(j>=5&&j<=10){
+                            array[i,j]="0";
+                        }
+                        break;
+                    }
                 break;
                 case 3:
-                    if (j==5||j==6||j==9||j==10){
-                    array[i,j]="0";
-                }
+                    if(i==2||i==6){
+                        if(j>=6&&j<=9){
+                            array[i,j]="0";
+                        }
+                    }
+                    else if(i==3||i==5){
+                        if(j==5||j==6||j==9||j==10){
+                            array[i,j]="0";
+                        }
+                    }
+                    else if(i==4){
+                        if(j>=8&&j<=10){
+                            array[i,j]="0";
+                        }
+                    }
                 break;
                 case 4:
-                    if(j==8||j==9){
-                    array[i,j]="0";
-                }
+                    if(i==2||i==3){
+                        if(j==5||j==6||j==9||j==10){
+                            array[i,j]="0";
+                        }
+                    }
+                    else if(i==4){
+                        if(j>=5&&j<=10){
+                            array[i,j]="0";
+                        }
+                    }
+                    else if(i==5||i==6){
+                        if (j==9||j==10){
+                            array[i,j]="0";
+                        }
+                    }
                 break;
                 case 5:
-                    if(j==7||j==8){
-                    array[i,j]="0";
-                }
+                    if(i==2||i==4||i==6){
+                        if(j>=5&&j<=10){
+                            array[i,j]="0";
+                        }
+                    }
+                    else if(i==3){
+                        if(j==5||j==6){
+                            array[i,j]="0";
+                        }
+                    }
+                    else if(i==5){
+                        if(j==10){
+                            array[i,j]="0";
+                        }
+                    }
                 break;
                 case 6:
-                    if(j>=5&&j<=10){
-                    array[i,j]="0";
-                }
+                    if(i==2){
+                        if(j==7||j==8){
+                            array[i,j]="0";
+                        }
+                    }
+                    if(i==3){
+                        if(j==6||j==7){
+                            array[i,j]="0";
+                        }
+                    }
+                    if(i==4||i==6){
+                        if(j>=5&&j<=9){
+                            array[i,j]="0";
+                        }
+                    }
+                    if(i==5){
+                        if(j==4||j==5||j==10){
+                            array[i,j]="0";
+                        }
+                    }
                 break;
             }
-        }
-
-        internal static void ConstructDie_Conditions3(int i, int j,ref string[,] array,int height=8,int width=16){
-            if(i==2||i==6){
-                if(j>=6&&j<=9){
-                    array[i,j]="0";
+            /*if(x==1){                             //I'm not sure if switch is actually better here :\
+                if (i==2||i==4||i==5){
+                    if(j>=7&&j<=8){
+                        array[i,j]="0";
+                    }
+                }
+                if(i==3){
+                    if(j>=5&&j<=8){
+                        array[i,j]="0";
+                    }
+                }
+                if(i==6){
+                    if(j>=5&&j<=10){
+                        array[i,j]="0";
+                    }
                 }
             }
-            else if(i==3||i==5){
-                if(j==5||j==6||j==9||j==10){
-                    array[i,j]="0";
+            if(x==2){
+                switch(i){
+                    case 2:
+                        if(j>=6&&j<=9){
+                        array[i,j]="0";
+                    }
+                    break;
+                    case 3:
+                        if (j==5||j==6||j==9||j==10){
+                        array[i,j]="0";
+                    }
+                    break;
+                    case 4:
+                        if(j==8||j==9){
+                        array[i,j]="0";
+                    }
+                    break;
+                    case 5:
+                        if(j==7||j==8){
+                        array[i,j]="0";
+                    }
+                    break;
+                    case 6:
+                        if(j>=5&&j<=10){
+                        array[i,j]="0";
+                    }
+                    break;
                 }
             }
-            else if(i==4){
-                if(j>=8&&j<=10){
-                    array[i,j]="0";
+            if(x==3){
+                if(i==2||i==6){
+                    if(j>=6&&j<=9){
+                        array[i,j]="0";
+                    }
+                }
+                else if(i==3||i==5){
+                    if(j==5||j==6||j==9||j==10){
+                        array[i,j]="0";
+                    }
+                }
+                else if(i==4){
+                    if(j>=8&&j<=10){
+                        array[i,j]="0";
+                    }
                 }
             }
-        }
-        internal static void ConstructDie_Conditions4(int i, int j,ref string[,] array,int height=8,int width=16){
-            if(i==2||i==3){
-                if(j==5||j==6||j==9||j==10){
-                    array[i,j]="0";
+            if(x==4){
+                if(i==2||i==3){
+                    if(j==5||j==6||j==9||j==10){
+                        array[i,j]="0";
+                    }
+                }else if(i==4){
+                    if(j>=5&&j<=10){
+                        array[i,j]="0";
+                    }
                 }
-            }else if(i==4){
-                if(j>=5&&j<=10){
-                    array[i,j]="0";
-                }
-            }
-            else if(i==5||i==6){
-                if (j==9||j==10){
-                    array[i,j]="0";
-                }
-            }
-        }
-        internal static void ConstructDie_Conditions5(int i, int j,ref string[,] array,int height=8,int width=16){
-            if(i==2||i==4||i==6){
-                if(j>=5&&j<=10){
-                    array[i,j]="0";
+                else if(i==5||i==6){
+                    if (j==9||j==10){
+                        array[i,j]="0";
+                    }
                 }
             }
-            else if(i==3){
-                if(j==5||j==6){
-                    array[i,j]="0";
+            if(x==5){
+                if(i==2||i==4||i==6){
+                    if(j>=5&&j<=10){
+                        array[i,j]="0";
+                    }
+                }
+                else if(i==3){
+                    if(j==5||j==6){
+                        array[i,j]="0";
+                    }
+                }
+                else if(i==5){
+                    if(j==10){
+                        array[i,j]="0";
+                    }
                 }
             }
-            else if(i==5){
-                if(j==10){
-                    array[i,j]="0";
+            if(x==6){
+                if(i==2){
+                    if(j==7||j==8){
+                        array[i,j]="0";
+                    }
                 }
-            }
-        }
-        internal static void ConstructDie_Conditions6(int i, int j,ref string[,] array,int height=8,int width=16){
-            if(i==2){
-                if(j==7||j==8){
-                    array[i,j]="0";
+                if(i==3){
+                    if(j==6||j==7){
+                        array[i,j]="0";
+                    }
                 }
-            }
-            if(i==3){
-                if(j==6||j==7){
-                    array[i,j]="0";
+                if(i==4||i==6){
+                    if(j>=5&&j<=9){
+                        array[i,j]="0";
+                    }
                 }
-            }
-            if(i==4||i==6){
-                if(j>=5&&j<=9){
-                    array[i,j]="0";
+                if(i==5){
+                    if(j==4||j==5||j==10){
+                        array[i,j]="0";
+                    }
                 }
-            }
-            if(i==5){
-                if(j==4||j==5||j==10){
-                    array[i,j]="0";
-                }
-            }
+            }*/
         }
     }
 }
