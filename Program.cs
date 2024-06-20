65ba7a6f35ee035cdb98087e7ebb3d6cbc78a68f
ostracized605 <kolco0202@gmail.com>
2024-06-20 12:52:15 +0700

    changed name

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiceThrowing{
+    class Project{
+        
+        static void Main(){
+            //Define color to have every one of 16 colours.
+            ConsoleColor[] color=new ConsoleColor[16];
+            color=Colours.Clrs();
+            //Create 6 dice
+            Die die=new Die();
+            Die.DieConstructor(ref Die.dice);
+            Game gm = new Game();
+            //Enter the number of dice
+            System.Console.WriteLine("How many dice do you want to throw? Max number is 3.");
+            gm.DiceChosen=gm.Choosing();
+            //Output result
+            int[] array=gm.Throwing(gm.DiceChosen);
+            Console.ReadLine();
+        }
+    }
+}
+        
